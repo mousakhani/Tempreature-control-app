7d0fcceb736d9eda54f80624412106cffb01d934
mousa <khodemousa@gmail.com>
2022-07-09 23:44:54 +0430

    initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./css/index.css";
+import Temperature from "./components/Temperature";
+
+function App() {
+  return <div className=""></div>;
+}
+export default App;
